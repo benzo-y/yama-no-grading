a5ec7b1c1cde4c97501fa3d3a0ee3ddc34fd5739
benzo-y <fukubenzo@gmail>
2022-02-16 11:55:20 +0900

    #1 ページのひな形とルーティングを設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/pages/Login.vue'
+import Matrix from '../components/pages/Matrix.vue'
+import Routes from '../components/pages/Routes.vue'
+import RouteForm from '../components/pages/RouteForm.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/matrix',
+    name: 'matrix',
+    component: Matrix
+  },
+  {
+    path: '/routes',
+    name: 'routes',
+    component: Routes
+  },
+  {
+    path: '/routes/:route_id?/edit',
+    name: 'route_edit',
+    component: RouteForm
+  },
 ]
 
 const router = new VueRouter({
