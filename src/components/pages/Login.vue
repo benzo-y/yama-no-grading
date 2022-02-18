5ece4cafe09af626a469b164ad59cdc11614d5b4
benzo-y <fukubenzo@gmail>
2022-02-18 11:22:24 +0900

    #3 ログイン画面のひな形作成

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>This is a Login page</h1>
-  </div>
+  <v-container text-center>
+    <v-row justify="center">
+      <v-col cols="12">
+        <h1>山のグレーディング</h1>
+      </v-col>
+      <v-col cols="8">
+        <div>
+          <p>
+            各自治体の山のグレーディングをまとめたサイトです。<br>Googleアカウントでログインしてください。
+          </p>
+        </div>
+        <div>
+          <v-btn color="info">
+            Googleアカウントでログイン
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
