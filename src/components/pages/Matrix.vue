4e884b3ce7aa2d1f44170e9c9c809ddc3389c68a
benzo-y <fukubenzo@gmail.com>
2022-03-03 12:15:33 +0900

    #10 マトリクス画面の横軸・縦軸にツールチップを表示

--- src/components/pages/Matrix.vue
+++ src/components/pages/Matrix.vue
@@ -7,25 +7,29 @@
           :changeClimbed.sync="climbedValue"
         />
       </v-col>
-      <v-col cols="1">
-        <v-card height="100%">
-          体力度
-        </v-card>
-      </v-col>
-      <v-col cols="11">
-        <v-row v-for="phy in physical" :key="phy">
+      <v-col cols="12">
+        <v-row v-for="phy in physical" :key="phy.grade">
           <v-col cols="1">
-            <v-card height="100%">
-              {{phy}}
-            </v-card>
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-card
+                  height="100%"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  体力度：{{phy.grade}}
+                </v-card>
+              </template>
+              <span>{{phy.tooltip}}</span>
+            </v-tooltip>
           </v-col>
           <v-col
             v-for="tech in technological"
-            :key="`${phy}${tech}`"
+            :key="`${phy.grade}${tech.grade}`"
             class="col-11-5"
           >
             <CardRouteListVue
-              :routes="createRouteList(phy, tech)"
+              :routes="createRouteList(phy.grade, tech.grade)"
               :getSelectedRouteId.sync="dialog"
             />
           </v-col>
@@ -33,24 +37,26 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="1"></v-col>
-      <v-col cols="11">
+      <v-col cols="12">
         <v-row>
           <v-col cols="1"></v-col>
           <v-col
             v-for="tech in technological"
-            :key="tech"
+            :key="tech.grade"
             class="col-11-5"
           >
-            <v-card>
-              {{tech}}
-            </v-card>
-          </v-col>
-          <v-col cols="1"></v-col>
-          <v-col cols="11">
-            <v-card>
-              技術的難易度
-            </v-card>
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <v-card
+                  height="100%"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  技術的難易度：{{tech.grade}}
+                </v-card>
+              </template>
+              <span v-html="tech.tooltip"></span>
+            </v-tooltip>
           </v-col>
         </v-row>
       </v-col>
@@ -80,8 +86,64 @@ export default {
     data: () => ({
       routes: [],
       matrix: new Map(),
-      physical: [1, 2 ,3 ,4 ,5 ,6 ,7 ,8 , 9, 10],
-      technological: ["A", "B", "C", "D", "E"],
+      physical: [
+        { grade: 10, tooltip: "2～3泊以上が適当" },
+        { grade:  9, tooltip: "2～3泊以上が適当" },
+        { grade:  8, tooltip: "2～3泊以上が適当" },
+        { grade:  7, tooltip: "1～2泊以上が適当" },
+        { grade:  6, tooltip: "1～2泊以上が適当" },
+        { grade:  5, tooltip: "1泊以上が適当" },
+        { grade:  4, tooltip: "1泊以上が適当" },
+        { grade:  3, tooltip: "日帰りが可能" },
+        { grade:  2, tooltip: "日帰りが可能" },
+        { grade:  1, tooltip: "日帰りが可能" },
+      ],
+      technological: [
+        { grade: "A", tooltip: `登山道<br>
+                                ◇概ね整備済<br>
+                                ◇転んだ場合でも転落・滑落の可能性は低い。<br>
+                                ◇道迷いの心配は少ない。<br>
+                                <br>
+                                技術・能力<br>
+                                ◇登山の装備が必要`
+        },
+        { grade: "B", tooltip: `登山道<br>
+                                ◇沢、崖、場所により雪渓などを通過<br>
+                                ◇急な登下降がある。<br>
+                                ◇道が分かりにくい所がある。<br>
+                                ◇転んだ場合の転落・滑落事故につながる場所がある。<br>
+                                <br>
+                                技術・能力<br>
+                                ◇登山経験が必要<br>
+                                ◇地図読み能力があることが望ましい。`
+        },
+        { grade: "C", tooltip: `登山道<br>
+                                ◇ハシゴ・くさり場、また、場所により雪渓や渡渉箇所がある。<br>
+                                ◇ミスをすると転落・滑落などの事故になる場所がある。<br>
+                                ◇案内標識が不十分な箇所も含まれる。<br>
+                                <br>
+                                技術・能力<br>
+                                ◇地図読み能力、ハシゴ・くさり場などを通過できる身体能力が必要`
+        },
+        { grade: "D", tooltip: `登山道<br>
+                                ◇厳しい岩稜や不安定なガレ場、ハシゴ・くさり場、藪漕ぎを必要とする箇所、場所により雪渓や渡渉箇所がある。<br>
+                                ◇手を使う急な登下降がある。<br>
+                                ◇ハシゴ・くさり場や案内標識などの人工的な補助は限定的で、転落・滑落の危険箇所が多い。<br>
+                                <br>
+                                技術・能力<br>
+                                ◇地図読み能力、岩場、雪渓を安定して通過できるバランス能力や技術が必要<br>
+                                ◇ルートファインディングの技術が必要`
+        },
+        { grade: "E", tooltip: `登山道<br>
+                                ◇緊張を強いられる厳しい岩稜の登下降が続き、転落・滑落の危険箇所が連続する。<br>
+                                ◇深い藪漕ぎを必要とする箇所が連続する場合がある。<br>
+                                <br>
+                                技術・能力<br>
+                                ◇地図読み能力、岩場、雪渓を安定して通過できるバランス能力や技術が必要<br>
+                                ◇ルートファインディングの技術、高度な判断力が必要<br>
+                                ◇登山者によってはロープを使わないと危険な場所もある`
+        },
+      ],
       locationValue: [],
       climbedValue: [],
       dialog: {
