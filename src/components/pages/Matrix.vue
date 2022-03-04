943634e72b05d27c42b3fbac794eba2292cbbf80
benzo-y <fukubenzo@gmail.com>
2022-03-04 09:56:39 +0900

    #10 マトリクス画面の横軸・縦軸の内容を定数ファイルに移動

--- src/components/pages/Matrix.vue
+++ src/components/pages/Matrix.vue
@@ -17,7 +17,7 @@
                   v-bind="attrs"
                   v-on="on"
                 >
-                  体力度：{{phy.grade}}
+                  {{phy.body}}
                 </v-card>
               </template>
               <span>{{phy.tooltip}}</span>
@@ -52,7 +52,7 @@
                   v-bind="attrs"
                   v-on="on"
                 >
-                  技術的難易度：{{tech.grade}}
+                  {{tech.body}}
                 </v-card>
               </template>
               <span v-html="tech.tooltip"></span>
@@ -80,70 +80,15 @@
 import CardRouteListVue from "./matrix/CardRouteList.vue";
 import CardFilterVue from "./matrix/CardFilter.vue";
 import CardRouteForm from "../parts/CardRouteForm.vue"
+import { MATRIX_AXIS } from "../../const/const";
 import { mapGetters } from "vuex"
 
 export default {
     data: () => ({
       routes: [],
       matrix: new Map(),
-      physical: [
-        { grade: 10, tooltip: "2～3泊以上が適当" },
-        { grade:  9, tooltip: "2～3泊以上が適当" },
-        { grade:  8, tooltip: "2～3泊以上が適当" },
-        { grade:  7, tooltip: "1～2泊以上が適当" },
-        { grade:  6, tooltip: "1～2泊以上が適当" },
-        { grade:  5, tooltip: "1泊以上が適当" },
-        { grade:  4, tooltip: "1泊以上が適当" },
-        { grade:  3, tooltip: "日帰りが可能" },
-        { grade:  2, tooltip: "日帰りが可能" },
-        { grade:  1, tooltip: "日帰りが可能" },
-      ],
-      technological: [
-        { grade: "A", tooltip: `登山道<br>
-                                ◇概ね整備済<br>
-                                ◇転んだ場合でも転落・滑落の可能性は低い。<br>
-                                ◇道迷いの心配は少ない。<br>
-                                <br>
-                                技術・能力<br>
-                                ◇登山の装備が必要`
-        },
-        { grade: "B", tooltip: `登山道<br>
-                                ◇沢、崖、場所により雪渓などを通過<br>
-                                ◇急な登下降がある。<br>
-                                ◇道が分かりにくい所がある。<br>
-                                ◇転んだ場合の転落・滑落事故につながる場所がある。<br>
-                                <br>
-                                技術・能力<br>
-                                ◇登山経験が必要<br>
-                                ◇地図読み能力があることが望ましい。`
-        },
-        { grade: "C", tooltip: `登山道<br>
-                                ◇ハシゴ・くさり場、また、場所により雪渓や渡渉箇所がある。<br>
-                                ◇ミスをすると転落・滑落などの事故になる場所がある。<br>
-                                ◇案内標識が不十分な箇所も含まれる。<br>
-                                <br>
-                                技術・能力<br>
-                                ◇地図読み能力、ハシゴ・くさり場などを通過できる身体能力が必要`
-        },
-        { grade: "D", tooltip: `登山道<br>
-                                ◇厳しい岩稜や不安定なガレ場、ハシゴ・くさり場、藪漕ぎを必要とする箇所、場所により雪渓や渡渉箇所がある。<br>
-                                ◇手を使う急な登下降がある。<br>
-                                ◇ハシゴ・くさり場や案内標識などの人工的な補助は限定的で、転落・滑落の危険箇所が多い。<br>
-                                <br>
-                                技術・能力<br>
-                                ◇地図読み能力、岩場、雪渓を安定して通過できるバランス能力や技術が必要<br>
-                                ◇ルートファインディングの技術が必要`
-        },
-        { grade: "E", tooltip: `登山道<br>
-                                ◇緊張を強いられる厳しい岩稜の登下降が続き、転落・滑落の危険箇所が連続する。<br>
-                                ◇深い藪漕ぎを必要とする箇所が連続する場合がある。<br>
-                                <br>
-                                技術・能力<br>
-                                ◇地図読み能力、岩場、雪渓を安定して通過できるバランス能力や技術が必要<br>
-                                ◇ルートファインディングの技術、高度な判断力が必要<br>
-                                ◇登山者によってはロープを使わないと危険な場所もある`
-        },
-      ],
+      physical: MATRIX_AXIS.physical,
+      technological: MATRIX_AXIS.technological,
       locationValue: [],
       climbedValue: [],
       dialog: {
