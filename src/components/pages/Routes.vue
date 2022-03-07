ce6077bc712d0c269b37c4b5118ba46d005259be
benzo-y <fukubenzo@gmail.com>
2022-03-07 11:17:58 +0900

    #15 インデントを調整

--- src/components/pages/Routes.vue
+++ src/components/pages/Routes.vue
@@ -64,68 +64,68 @@
 import { mapGetters } from "vuex";
 import IconClimbed from "../parts/IconClimbed.vue";
 
-  export default {
-    data: () => ({
-      search: '',
-      physical: '',
-      routes: [],
-    }),
-    components: {
-      IconClimbed,
-    },
-    computed: {
-      ...mapGetters(["routeMap"]),
-      headers () {
-        return [
-          {
-            text: 'ルート名称',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          {
-            text: '体力度',
-            value: 'physical',
-            filter: value => {
-              if (!this.physical) return true
+export default {
+  data: () => ({
+    search: '',
+    physical: '',
+    routes: [],
+  }),
+  components: {
+    IconClimbed,
+  },
+  computed: {
+    ...mapGetters(["routeMap"]),
+    headers () {
+      return [
+        {
+          text: 'ルート名称',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        {
+          text: '体力度',
+          value: 'physical',
+          filter: value => {
+            if (!this.physical) return true
 
-              return value < parseInt(this.physical)
-            },
+            return value < parseInt(this.physical)
           },
-          { text: '技術的難易度', value: 'technological' },
-          { text: 'スタート地点（地名）', value: 'start_point_name' },
-          { text: 'スタート地点（標高）', value: 'start_point_elevation' },
-          { text: 'ルート最高地点（地名）', value: 'highest_point_name' },
-          { text: 'ルート最高地点（標高）', value: 'highest_point_elevation' },
-          { text: '終了地点（地名）', value: 'end_point_name' },
-          { text: '終了地点（標高）', value: 'end_point_elevation' },
-          { text: 'コースタイム', value: 'course_time' },
-          { text: 'ルート長', value: 'length' },
-          { text: '累計登り標高差', value: 'cum_up_elevation' },
-          { text: '累計下り標高差', value: 'cum_down_elevation' },
-          { text: 'ルート係数', value: 'route_coef' },
-          { text: '発行元', value: 'publisher' },
-          { text: '登頂チェック', value: 'climbed' },
-          { text: 'アクション', value: 'actions' },
-        ]
-      },
+        },
+        { text: '技術的難易度', value: 'technological' },
+        { text: 'スタート地点（地名）', value: 'start_point_name' },
+        { text: 'スタート地点（標高）', value: 'start_point_elevation' },
+        { text: 'ルート最高地点（地名）', value: 'highest_point_name' },
+        { text: 'ルート最高地点（標高）', value: 'highest_point_elevation' },
+        { text: '終了地点（地名）', value: 'end_point_name' },
+        { text: '終了地点（標高）', value: 'end_point_elevation' },
+        { text: 'コースタイム', value: 'course_time' },
+        { text: 'ルート長', value: 'length' },
+        { text: '累計登り標高差', value: 'cum_up_elevation' },
+        { text: '累計下り標高差', value: 'cum_down_elevation' },
+        { text: 'ルート係数', value: 'route_coef' },
+        { text: '発行元', value: 'publisher' },
+        { text: '登頂チェック', value: 'climbed' },
+        { text: 'アクション', value: 'actions' },
+      ]
     },
-    mounted() {
-      this.routes = this.routeMap.get("all");
-      // stateの値の変更を検知する（ミューテーション実行後の値を取得）
-      this.$store.subscribe((mutation) => {
-        if (mutation.type === 'setRouteMap') {
-          this.routes = this.routeMap.get("all");
-        }
-      });
+  },
+  mounted() {
+    this.routes = this.routeMap.get("all");
+    // stateの値の変更を検知する（ミューテーション実行後の値を取得）
+    this.$store.subscribe((mutation) => {
+      if (mutation.type === 'setRouteMap') {
+        this.routes = this.routeMap.get("all");
+      }
+    });
+  },
+  methods: {
+    filterOnlyCapsText (value, search) {
+      return value != null &&
+        search != null &&
+        typeof value === 'string' &&
+        value.toString().toLocaleUpperCase().indexOf(search) !== -1
     },
-    methods: {
-      filterOnlyCapsText (value, search) {
-        return value != null &&
-          search != null &&
-          typeof value === 'string' &&
-          value.toString().toLocaleUpperCase().indexOf(search) !== -1
-      },
-    },
-  }
+  },
+}
 </script>
