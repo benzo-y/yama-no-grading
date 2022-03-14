d07e1899a88148beb4458d57e36421b808106a99
benzo-y <fukubenzo@gmail.com>
2022-03-14 11:51:48 +0900

    #18 ルート登録・変更画面の各入力項目と入力チェック実装

--- src/components/parts/CardRouteForm.vue
+++ src/components/parts/CardRouteForm.vue
@@ -10,23 +10,62 @@
     </v-card-title>
     <v-card-text>
       <v-form
-        v-for="item, property in label"
-        :key="property"
+        ref="form"
         class="mx-4"
       >
-        <v-text-field
-          v-if="mode==='edit'"
-          v-model="route[property]"
-          :label="item"
+        <template
+          v-for="item, property in form"
         >
-        </v-text-field>
-        <v-text-field
-          v-if="mode==='read'"
-          v-model="route[property]"
-          :label="item"
-          readonly
-        >
-        </v-text-field>
+          <v-text-field
+            v-if="item.type==='text'"
+            v-model="route[property]"
+            :label="item.label"
+            :rules="item.rules"
+            :readonly="mode==='read'"
+            :key="property"
+          >
+          </v-text-field>
+          <v-text-field
+            v-if="item.type==='number'"
+            type="number"
+            min="0"
+            :max="item.max"
+            v-model.number="route[property]"
+            :label="item.label"
+            :rules="item.rules"
+            :readonly="mode==='read'"
+            :key="property"
+          >
+          </v-text-field>
+          <v-text-field
+            v-if="item.type==='auto'"
+            v-model.number="autoInputValues[property]"
+            :label="item.label"
+            readonly
+            placeholder="自動で計算されます。"
+            :key="property"
+          >
+          </v-text-field>
+          <v-select
+            v-if="item.type==='select'"
+            :items="item.items"
+            v-model="route[property]"
+            :label="item.label"
+            :rules="item.rules"
+            :readonly="mode==='read'"
+            :key="property"
+          >
+          </v-select>
+          <v-text-field
+            v-if="!item.type && mode==='read'"
+            v-model="route[property]"
+            :label="item.label"
+            :rules="item.rules"
+            readonly
+            :key="property"
+          >
+          </v-text-field>
+        </template>
       </v-form>
     </v-card-text>
     <v-card-actions class="justify-center">
@@ -51,22 +90,111 @@ import { mapGetters } from "vuex"
 export default {
   components: {IconClimbed},
   data: () => ({
-    label: {
-      name : 'ルート名称',
-      physical : '体力度',
-      technological : '技術的難易度',
-      start_point_name : 'スタート地点 地名',
-      start_point_elevation : 'スタート地点 標高',
-      highest_point_name : '最高地点 名称',
-      highest_point_elevation : '最高地点 標高',
-      end_point_name : '終了地点 名称',
-      end_point_elevation : '終了地点 標高',
-      course_time : 'コースタイム',
-      length : 'ルート長',
-      cum_up_elevation : '累計登り標高差',
-      cum_down_elevation : '累計下り標高差',
-      route_coef : 'ルート係数',
-      publisher : '発行元',
+    form: {
+      name : {
+        label: 'ルート名称',
+        type: 'text',
+        rules: [
+          v => (v && v.length <= 100) || '100文字以内で値を入力してください。',
+        ],
+      },
+      physical : {
+        label:'体力度',
+        type: 'auto',
+        valueName: ''
+      },
+      technological : {
+        label:'技術的難易度',
+        type: 'select',
+        items: ["A","B","C","D","E"],
+        rules: [
+          v => !!v || '選択してください。',
+        ],
+      },
+      start_point_name : {
+        label:'スタート地点 地名',
+        type: 'text',
+        rules: [
+          v => (v && v.length <= 100) || '100文字以内で値を入力してください。',
+        ],
+      },
+      start_point_elevation : {
+        label:'スタート地点 標高（m）',
+        type: 'number',
+        max: 8848,
+        rules: [
+          v => (1 <= v && v <= 8848) || '1～8848の範囲で値を入力してください。',
+        ],
+        },
+      highest_point_name : {
+        label:'最高地点 名称',
+        type: 'text',
+        rules: [
+          v => (v && v.length <= 100) || '100文字以内で値を入力してください。',
+        ],
+      },
+      highest_point_elevation : {
+        label:'最高地点 標高（m）',
+        type: 'number',
+        max: 8848,
+        rules: [
+          v => (1 <= v && v <= 8848) || '1～8848の範囲で値を入力してください。',
+        ],
+      },
+      end_point_name : {
+        label:'終了地点 名称',
+        type: 'text',
+        rules: [
+          v => (v && v.length <= 100) || '100文字以内で値を入力してください。',
+        ],
+      },
+      end_point_elevation : {
+        label:'終了地点 標高（m）',
+        type: 'number',
+        max: 8848,
+        rules: [
+          v => (1 <= v && v <= 8848) || '1～8848の範囲で値を入力してください。',
+        ],
+      },
+      course_time : {
+        label:'コースタイム（h）',
+        type: 'number',
+        max: 1000,
+        rules: [
+          v => (1 <= v && v <= 1000) || '1～1000の範囲で値を入力してください。',
+        ],
+      },
+      length : {
+        label:'ルート長（km）',
+        type: 'number',
+        max: 10000,
+        rules: [
+          v => (1 <= v && v <= 10000) || '1～10000の範囲で値を入力してください。',
+        ],
+      },
+      cum_up_elevation : {
+        label:'累計登り標高差（km）',
+        type: 'number',
+        max: 1000,
+        rules: [
+          v => (1 <= v && v <= 1000) || '1～1000の範囲で値を入力してください。',
+        ],
+      },
+      cum_down_elevation : {
+        label:'累計下り標高差（km）',
+        type: 'number',
+        max: 1000,
+        rules: [
+          v => (1 <= v && v <= 1000) || '1～1000の範囲で値を入力してください。',
+        ],
+      },
+      route_coef : {
+        label:'ルート係数',
+        type: 'auto',
+      },
+      publisher : {
+        label:'発行元',
+      },
     },
     route: {},
     climbed: false
@@ -77,6 +205,15 @@ export default {
   },
   computed: {
     ...mapGetters(["getRouteById"]),
+    autoInputValues() {
+      let physical, route_coef
+      if(this.route.course_time && this.route.length && this.route.cum_up_elevation && this.route.cum_down_elevation) {
+        route_coef = this.route.course_time*1.8 + this.route.length*0.3 +
+          this.route.cum_up_elevation*10.0 + this.route.cum_down_elevation*0.6;
+        physical = route_coef/10 < 10 ? Math.floor(route_coef/10) : 10;
+      }
+      return { physical, route_coef };
+    }
   },
   created() {
     if (this.id) {
@@ -99,7 +236,9 @@ export default {
       this.$emit('update:clickCancel');
     },
     clickOk() {
-      this.$emit('update:clickOk', this.route);
+      if(this.$refs.form.validate()) {
+        this.$emit('update:clickOk', {...this.route, ...this.autoInputValues });
+      }
     },
   },
 }
